d8ca303e036bb84bbe8dd063463d56236fe7f860
danilo-mir <danilo.mir271@gmail.com>
2025-03-21 15:35:48 -0300

    Feat: implementation of signal for asset verification task deletion

--- app/signals.py
+++ app/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Assets
 from django.utils import timezone
@@ -10,8 +10,6 @@ import json
 def new_asset_verification_task(sender, instance, created, **kwargs):
     if created:
 
-        print(f"signal criado para ativo {instance.ticker}")
-
         interval, _ = IntervalSchedule.objects.get_or_create(
             every=instance.check_period,
             period=IntervalSchedule.MINUTES,
@@ -19,9 +17,19 @@ def new_asset_verification_task(sender, instance, created, **kwargs):
 
         PeriodicTask.objects.create(
             interval=interval,
-            name=f'Verify asset {instance.ticker}',
-            task='app.tasks.price_tunnel_check',
-            kwargs=json.dumps({'asset_ticker': instance.ticker}),
+            name=f"{instance.id}",
+            task="app.tasks.price_tunnel_check",
+            kwargs=json.dumps({"asset_ticker": instance.ticker}),
             start_time=timezone.now(),
             enabled=True
         )
+
+
+@receiver(post_delete, sender=Assets)
+def delete_asset_verification_task(sender, instance, **kwargs):
+    task_name = f"{instance.id}"
+    task = PeriodicTask.objects.filter(name=task_name)
+    if task.exists():
+        task.delete()
+    else:
+        print(f"[Info] Failed to delete task {task_name}: task already doesn't exist")
